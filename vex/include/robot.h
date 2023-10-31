6924f6dc81c7a2d15a98b9f9f286fc988ba7f369
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2023-10-30 21:06:24 -0400

    Robot created

--- vex/include/robot.h
+++ vex/include/robot.h
@@ -1,20 +1,45 @@
 //define robot ports here
+#pragma once
+
 #include "api.h"
 #include "library/ChassisController.h"
 
-#define FRONT_RIGHT_MOTOR_PORT 10
-#define FRONT_LEFT_MOTOR_PORT  8
-#define BACK_RIGHT_MOTOR_PORT  20
-#define BACK_LEFT_MOTOR_PORT   17
+#define RIGHT_MOTOR_1_PORT 1
+#define RIGHT_MOTOR_2_PORT 3
+#define RIGHT_MOTOR_3_PORT 4
+#define RIGHT_MOTOR_4_PORT 5
+
+#define LEFT_MOTOR_1_PORT 7
+#define LEFT_MOTOR_2_PORT 8
+#define LEFT_MOTOR_3_PORT 9
+#define LEFT_MOTOR_4_PORT 10
+
+#define CATA_ONE_PORT 11
+#define CATA_TWO_PORT 20
+#define CATA_ROT_SENSE_PORT 13
+
+#define INTAKE_PORT 12
 
 #define INERTIAL_PORT          6
 
-extern pros::Motor f_rightMotor;
-extern pros::Motor f_leftMotor;
-extern pros::Motor b_rightMotor;
-extern pros::Motor b_leftMotor;
+extern pros::Motor rightMotor1;
+extern pros::Motor rightMotor2;
+extern pros::Motor rightMotor3;
+extern pros::Motor rightMotor4;
+
+extern pros::Motor leftMotor1;
+extern pros::Motor leftMotor2;
+extern pros::Motor leftMotor3;
+extern pros::Motor leftMotor4;
 
 extern pros::Motor_Group leftMotors;
 extern pros::Motor_Group rightMotors;
 
-extern pros::Imu gyro;
+extern pros::Motor cata1;
+extern pros::Motor cata2;
+extern pros::Motor_Group cata_motors;
+extern pros::Rotation cataSensor;
+
+extern pros::Motor IntakeMotor;
+
+extern pros::Imu imu;
--- vex/src/robot_init.cpp
+++ vex/src/robot_init.cpp
@@ -1,12 +1,25 @@
 //initialize robot components here
 #include "robot.h"
 
-pros::Motor f_rightMotor = pros::Motor(FRONT_RIGHT_MOTOR_PORT, true);
-pros::Motor f_leftMotor = pros::Motor(FRONT_LEFT_MOTOR_PORT);
-pros::Motor b_rightMotor = pros::Motor(BACK_RIGHT_MOTOR_PORT, true);
-pros::Motor b_leftMotor = pros::Motor(BACK_LEFT_MOTOR_PORT);
+pros::Motor rightMotor1 = pros::Motor(RIGHT_MOTOR_1_PORT, true);
+pros::Motor rightMotor2 = pros::Motor(RIGHT_MOTOR_2_PORT, true);
+pros::Motor rightMotor3 = pros::Motor(RIGHT_MOTOR_3_PORT, true);
+pros::Motor rightMotor4 = pros::Motor(RIGHT_MOTOR_4_PORT, true);
 
-pros::Motor_Group leftMotors = pros::Motor_Group({f_leftMotor, b_leftMotor});
-pros::Motor_Group rightMotors = pros::Motor_Group({f_rightMotor, b_rightMotor});
+pros::Motor leftMotor1 = pros::Motor(LEFT_MOTOR_1_PORT);
+pros::Motor leftMotor2 = pros::Motor(LEFT_MOTOR_2_PORT);
+pros::Motor leftMotor3 = pros::Motor(LEFT_MOTOR_3_PORT);
+pros::Motor leftMotor4 = pros::Motor(LEFT_MOTOR_4_PORT);
+
+pros::Motor_Group leftMotors = pros::Motor_Group({leftMotor1, leftMotor2, leftMotor3, leftMotor4});
+pros::Motor_Group rightMotors = pros::Motor_Group({rightMotor1, rightMotor2, rightMotor3, rightMotor4});
+
+pros::Motor cata1 = pros::Motor(CATA_ONE_PORT);
+pros::Motor cata2 = pros::Motor(CATA_TWO_PORT, true);
+pros::Motor_Group cata = pros::Motor_Group({cata1,cata2});
+
+pros::Rotation cataSensor = pros::Rotation(CATA_ROT_SENSE_PORT);
+
+pros::Motor IntakeMotor = pros::Motor(INTAKE_PORT);
 
 pros::Imu gyro = pros::Imu(INERTIAL_PORT);
