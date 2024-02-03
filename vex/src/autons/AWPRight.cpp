fd7d19c6de05e11310d6fcbe8a4411600abccde6
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2024-02-02 19:34:39 -0500

    Revived auton from the dead

--- vex/src/autons/AWPRight.cpp
+++ vex/src/autons/AWPRight.cpp
@@ -6,11 +6,7 @@ inline void runRightAwpAuton() {
     rightMotors.set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
 	leftMotors.set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
 
-    //pick up triball under bar and 
-
-    driveChassis.Move(4000, 90, 4, -1);
-
-    /*
+    //pick up triball under bar and back up
     startIntake();
     driveChassis.MovePid(700, 0.6, 4, true);
     driveChassis.MovePid(-1600, 0.6, 4, true);
@@ -25,5 +21,4 @@ inline void runRightAwpAuton() {
     //turn back towards the match load triball and move
     driveChassis.TurnPid(90, 1);
     driveChassis.MovePid(3000, 1, 5, true);
-    */
 }
