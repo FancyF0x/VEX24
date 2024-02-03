5117ad84a61ad3fa7377493d9248a2fe65ebc949
Andy Zhu <azhu625@gmail.com>
2024-02-03 12:24:36 -0500

    "auto" LMAOO

--- vex/src/autons/AWPRight.cpp
+++ vex/src/autons/AWPRight.cpp
@@ -13,42 +13,42 @@ inline void runRightAwpAuton() {
     pros::delay(200);
 
     //push the triball with the wings
-    driveChassis.MovePid(500, 1, -1, true);
-    driveChassis.TurnPid(50, 1.3, 0);
+    driveChassis.MovePid(1000, 1, -1, true);
+    // driveChassis.TurnPid(50, 1.3, 0);
 
-    driveChassis.Move(300, 127, -1, -1, true);
-    driveChassis.Move(-300, 127, -1, -1, true);
+    // driveChassis.Move(300, 127, -1, -1, true);
+    // driveChassis.Move(-300, 127, -1, -1, true);
 
-    //push the triball forward using the front wings
-    frontWingsDown();
+    // //push the triball forward using the front wings
+    // frontWingsDown();
 
-    driveChassis.Move(200, 60, -1, -1, true);
-    // backWingsUp();
-    driveChassis.Move(-350, 40, -1, -1, true);
+    // driveChassis.Move(200, 60, -1, -1, true);
+    // // backWingsUp();
+    // driveChassis.Move(-350, 40, -1, -1, true);
 
-    frontWingsUp();
-    driveChassis.TurnPid(-40, 1);
+    // frontWingsUp();
+    // driveChassis.TurnPid(-40, 1);
 
-    driveChassis.Move(350, 100, 6, 2000, false);
+    // driveChassis.Move(350, 100, 6, 2000, false);
     
-    //turn to face the goal and push the triball the rest of the way in
-    driveChassis.TurnPid(30, 1);
-    driveChassis.Move(240, 100, -1, 1000, false);
-    driveChassis.Move(-300, 127, 5, -1, true);
+    // //turn to face the goal and push the triball the rest of the way in
+    // driveChassis.TurnPid(30, 1);
+    // driveChassis.Move(240, 100, -1, 1000, false);
+    // driveChassis.Move(-300, 127, 5, -1, true);
 
-    //turn away from goal
-    driveChassis.TurnPid(125, 1);
+    // //turn away from goal
+    // driveChassis.TurnPid(125, 1);
 
-    //realign to field
-    // driveChassis.TurnPid(45-static_imu.get_rotation(), 1);
+    // //realign to field
+    // // driveChassis.TurnPid(45-static_imu.get_rotation(), 1);
     
-    //start raising hang bar
-    pros::Task([]() {
-    });
+    // //start raising hang bar
+    // pros::Task([]() {
+    // });
 
-    //drive to align with hanging bar
-    driveChassis.MovePid(900, 1, 4, true);
+    // //drive to align with hanging bar
+    // driveChassis.MovePid(900, 1, 4, true);
 
-    // driveChassis.TurnPid(-static_imu.get_rotation(), 1);
-    driveChassis.Move(2500, 80, 6, 5000);
+    // // driveChassis.TurnPid(-static_imu.get_rotation(), 1);
+    // driveChassis.Move(2500, 80, 6, 5000);
 }
