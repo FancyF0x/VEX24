630d3c0309c1a57f44e5fa1d89992a89687a54e3
Kyle DeGuzman <48830018+KyleADeGuzman@users.noreply.github.com>
2024-02-03 01:48:50 -0500

    rotation match load

--- vex/src/autons/AWPRight.cpp
+++ vex/src/autons/AWPRight.cpp
@@ -1,4 +1,5 @@
 #include "api.h"
+#include "pros/rtos.hpp"
 #include "robot.h"
 #include "autons/HelperMethods.h"
 
@@ -29,7 +30,7 @@ inline void runRightAwpAuton() {
     frontWingsUp();
     driveChassis.TurnPid(-40, 1);
 
-    driveChassis.Move(350, 100, 6, 2000, false);
+    driveChassis.Move(280, 100, 6, 2000, false);
     
     //turn to face the goal and push the triball the rest of the way in
     driveChassis.TurnPid(30, 1);
@@ -37,8 +38,48 @@ inline void runRightAwpAuton() {
     driveChassis.Move(-300, 127, 5, -1, true);
 
     //turn to align with corner
-    driveChassis.TurnPid(-45, 1);
-    driveChassis.Move(-300, 127, -1, -1, false);
+    driveChassis.TurnPid(-10, 1);
+    driveChassis.Move(-100, 100, -1, -1, false);
+    driveChassis.TurnPid(-15, 1);
+    driveChassis.Move(-100, 100, -1, -1, false);
+    driveChassis.TurnPid(-16, 1);
+    driveChassis.Move(-80, 100, -1, -1, false);
+    driveChassis.TurnPid(-21, 1);
+    driveChassis.Move(-100, 90, -1, -1, false);
+
+    //match load
+    backWingsDown();
+    pros::delay(1500);
+    driveChassis.Turn(-9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(-9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(9, 90);
+    pros::delay(1500); 
+    driveChassis.Turn(-9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(-9, 90);
+    pros::delay(1500);
+    driveChassis.Turn(9, 90);
+    pros::delay(1500);
+
+    backWingsUp();
+
+    //go to elevation
+    driveChassis.Move(-150, 90, -1, -1, false);
+    pros::delay(1000);
+    driveChassis.Turn(-35, 50);
+    pros::delay(600);
+    driveChassis.Move(-1600, 90, -1, -1, false);
+    
+    pros::delay(500);
+    // driveChassis.Turn(-15, 80);
+
+    // driveChassis.MovePid(-1200, 1, 4, true);
 
     // //turn away from goal
     // driveChassis.TurnPid(125, 1);
