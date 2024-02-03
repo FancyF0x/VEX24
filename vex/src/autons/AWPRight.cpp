e5ac887cff8ed29b3b28a89f4864815b81d96c10
Kahan-2410 <kahanjaveri2410@gmail.com>
2024-02-02 22:48:44 -0500

    auto

--- vex/src/autons/AWPRight.cpp
+++ vex/src/autons/AWPRight.cpp
@@ -1,6 +1,18 @@
 #include "api.h"
 #include "robot.h"
 
-// void runRightAwpAuton() {
+inline void runRightAwpAuton() {
 
-// }
+    rightMotors.set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
+	leftMotors.set_brake_modes(pros::E_MOTOR_BRAKE_BRAKE);
+
+    while (true) {
+        flywheelMotors.move(90);
+        intakeMotors.move(50);
+        intakeFold.move(90);
+    }
+
+    //push the triball with the wings
+    driveChassis.MovePid(500, 1, -1, true);
+    driveChassis.TurnPid(50, 1.3, 0);
+}
